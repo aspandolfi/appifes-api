e5f3338c67ed5a5c2d6bfa15e1720c64bb77b07d
André Serafim Pandolfi <aspandolfi@gmail.com>
2018-09-11 07:21:50 -0300

    Inicial

---
+++ CapVerify.Api/Models/TituloCaptalizacao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapVerify.Api.Models
+{
+    public class TituloCaptalizacao
+    {
+        public TituloCaptalizacao()
+        {
+
+        }
+
+        public int TituloCaptalizacaoId { get; set; }
+        public string Descricao { get; set; }
+        public string Jogo { get; set; }
+        public DateTime DataVencimento { get; set; }
+        public DateTime DataSorteio { get; set; }
+    }
+}
---
+++ CapVerify.Data/Context/CapVerifyContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapVerify.Data.Context
+{
+    public class CapVerifyContext : DbContext
+    {
+        public CapVerifyContext(DbContextOptions<CapVerifyContext> options)
+            : base(options)
+        {
+
+        }
+
+
+    }
+}
